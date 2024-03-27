3ed9ec17113591172b59e80398bb2c5be8b4a71f
Allonario <markush2004@mail.ru>
2024-03-27 17:35:44 +0300

    feat: replace create and edit page with upsert add product view manager

--- MVCWebApplication/Areas/Admin/Controllers/ProductController.cs
+++ MVCWebApplication/Areas/Admin/Controllers/ProductController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using MVC.DataAccess.Repository.IRepository;
 using MVC.Models;
+using MVC.Models.VueModels;
 
 namespace MVCWebApplication.Areas.Admin.Controllers
 {
@@ -8,9 +10,11 @@ namespace MVCWebApplication.Areas.Admin.Controllers
     public class ProductController : Controller
     {
         private readonly IUnitOfWork _unitOfWork;
-        public ProductController(IUnitOfWork unitOfWork)
+        private readonly IWebHostEnvironment _webHostEnvironment;
+        public ProductController(IUnitOfWork unitOfWork, IWebHostEnvironment webHostEnvironment)
         {
             _unitOfWork = unitOfWork;
+            _webHostEnvironment = webHostEnvironment;
         }
         public IActionResult Index()
         {
@@ -18,49 +22,77 @@ namespace MVCWebApplication.Areas.Admin.Controllers
             return View(objCategoryList);
         }
 
-        public IActionResult Create()
+        public IActionResult Upsert(int? id)
         {
-            return View();
+
+            ProductVm productVm = new()
+            {
+                CategoryList = _unitOfWork.CategoryRepository
+                .GetAll().Select(u => new SelectListItem
+                {
+                    Text = u.Name,
+                    Value = u.Id.ToString()
+                }),
+                Product = new Product()
+            };
+            if(id == null || id == 0)
+            {
+                return View(productVm);
+            }
+            else
+            {
+                productVm.Product = _unitOfWork.ProductRepository.Get(u=>u.Id == id);
+                return View(productVm);
+            }
         }
 
         [HttpPost]
-        public IActionResult Create(Product product)
+        public IActionResult Upsert(ProductVm productVm, IFormFile? file)
         {
             if (ModelState.IsValid)
             {
-                _unitOfWork.ProductRepository.Add(product);
+                string wwwRootPath = _webHostEnvironment.WebRootPath;
+                if(file != null)
+                {
+                    string fileName = Guid.NewGuid().ToString() + Path.GetExtension(file.FileName);
+                    string productPath = Path.Combine(wwwRootPath, @"img\product");
+
+                    if(!string.IsNullOrEmpty(productVm.Product.ImageUrl))
+                    {
+                        var oldImagePath = Path.Combine(wwwRootPath, productVm.Product.ImageUrl.TrimStart('\\'));
+                        if(System.IO.File.Exists(oldImagePath))
+                        {
+                            System.IO.File.Delete(oldImagePath);
+                        }
+                    }
+                    using(var fileStream = new FileStream(Path.Combine(productPath, fileName), FileMode.Create))
+                    {
+                        file.CopyTo(fileStream);
+                    }
+
+                    productVm.Product.ImageUrl = @"\img\product\" + fileName;
+                }
+                if(productVm.Product.Id == 0)
+                {
+                    _unitOfWork.ProductRepository.Add(productVm.Product);
+                }
+                else
+                {
+                    _unitOfWork.ProductRepository.Update(productVm.Product);
+                }
                 _unitOfWork.Save();
                 TempData["success"] = "Product created successfully";
                 return RedirectToAction("Index");
             }
-            return View();
-        }
-
-        public IActionResult Edit(int? id)
-        {
-            if (id == null || id == 0)
+            else
             {
-                return NotFound();
+                productVm.CategoryList = _unitOfWork.CategoryRepository.GetAll().Select(u => new SelectListItem
+                {
+                    Text = u.Name,
+                    Value = u.Id.ToString()
+                });
+                return View(productVm);
             }
-            Product? product = _unitOfWork.ProductRepository.Get(c => c.Id == id);
-            if (product == null)
-            {
-                return NotFound();
-            }
-            return View(product);
-        }
-
-        [HttpPost]
-        public IActionResult Edit(Product product)
-        {
-            if (ModelState.IsValid)
-            {
-                _unitOfWork.ProductRepository.Update(product);
-                _unitOfWork.Save();
-                TempData["success"] = "Category edited successfully";
-                return RedirectToAction("Index");
-            }
-            return View();
         }
 
         public IActionResult Delete(int? id)
@@ -69,12 +101,21 @@ namespace MVCWebApplication.Areas.Admin.Controllers
             {
                 return NotFound();
             }
-            Product? product = _unitOfWork.ProductRepository.Get(c => c.Id == id);
-            if (product == null)
+            ProductVm productVm = new()
+            {
+                CategoryList = _unitOfWork.CategoryRepository
+                .GetAll().Select(u => new SelectListItem
+                {
+                    Text = u.Name,
+                    Value = u.Id.ToString()
+                }),
+                Product = _unitOfWork.ProductRepository.Get(c => c.Id == id)
+            };
+            if (productVm.Product == null)
             {
                 return NotFound();
             }
-            return View(product);
+            return View(productVm);
         }
 
         [HttpPost, ActionName("Delete")]
@@ -84,6 +125,14 @@ namespace MVCWebApplication.Areas.Admin.Controllers
             if (product == null)
             {
                 return NotFound();
+            }
+            if (!string.IsNullOrEmpty(product.ImageUrl))
+            {
+                var oldImagePath = Path.Combine(_webHostEnvironment.WebRootPath, product.ImageUrl.TrimStart('\\'));
+                if (System.IO.File.Exists(oldImagePath))
+                {
+                    System.IO.File.Delete(oldImagePath);
+                }
             }
             _unitOfWork.ProductRepository.Remove(product);
             _unitOfWork.Save();
